f0cfb1ac0c54933f8889af132da76dce62fab525
Swati <swati308mmu@gmail.com>
2021-12-18 19:45:57 +0530

    waferdeaultprojectmodified

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="docker-heroku",
+    name="waferfaultdetection",
     version="0.0.3",
     description="ML project",
-    author="Avnish yadav", 
+    author="Swati",
     packages=find_packages(),
     license="MIT"
 )
